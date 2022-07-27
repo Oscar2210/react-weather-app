379db6a5d23fa4702d86f9b2f8de21b61df38928
Oscar Zambrano <Oscar2210@gmail.com>
2022-07-27 18:55:22 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f47a70c393mshf28a096f8f866d4p1b46a1jsnb2114d78d16b",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "10c67854a9e042f8086ed95587c1e20d";
